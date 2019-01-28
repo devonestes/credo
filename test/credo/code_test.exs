b314ceabaf4315421afa2cc54709154c940c1363
Devon Estes <devon.c.estes@gmail.com>
2019-01-28 08:42:57 +0100

    adding a slow test

--- test/credo/code_test.exs
+++ test/credo/code_test.exs
@@ -1,6 +1,30 @@
 defmodule Credo.CodeTest do
   use Credo.TestHelper
 
+  test "it should NOT report expected code (with sleep)" do
+    Process.sleep(500)
+    lines =
+      """
+      defmodule CredoSampleModule do
+        def some_function(parameter1, parameter2) do
+          some_value = parameter1 + parameter2
+        end
+      end
+      """
+      |> Credo.Code.to_lines()
+
+    expected = [
+      {1, "defmodule CredoSampleModule do"},
+      {2, "  def some_function(parameter1, parameter2) do"},
+      {3, "    some_value = parameter1 + parameter2"},
+      {4, "  end"},
+      {5, "end"},
+      {6, ""}
+    ]
+
+    assert expected == lines
+  end
+
   test "it should NOT report expected code" do
     lines =
       """
